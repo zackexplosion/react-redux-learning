8d90319490cf2552f26d98b136c54e904546d4ad
Tony's Macbook <cstony0917@gmail.com>
2016-05-29 18:26:19 +0800

    add redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { createStore, bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class App extends Component {
+// actions
+function changeText(e){
+  console.log('changeText')
+  console.log(e)
+  return {
+  	type: 'CHANGE_TEXT',
+  	payload: e.target.value
+  }
+}
+
+class TextFiled extends Component {
+  render(){
+  	let onChange = this.props.actions.changeText;
+  	return (<input type="text" onChange={onChange}/>)
+  }
+}
+
+class App extends Component {
   render() {
+  	let {actions, text} = this.props
     return (
-      <h1>Hello, world.</h1>
+      <div>
+      	<h1>Hello, {this.props.text}.</h1>
+      	<hr />
+      	<TextFiled actions={actions} text={text} />
+      </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+
+  return { text: state.text }
+}
+
+function mapDispatchToProps(dispatch) {
+  return{
+    actions: bindActionCreators({changeText:changeText}, dispatch)
+  }
+}
+
+App = connect(mapStateToProps, mapDispatchToProps)(App)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, bindActionCreators } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initState = {
+  text: 'world'
+}
+// reducer
+function myApp(state = initState, action){
+  console.log(state)
+  switch(action.type){
+  	case 'CHANGE_TEXT':
+  	  return {
+  	  	text: action.payload
+  	  }
+  	default:
+  	  return initState;
+  }
+}
+
+
+let store = createStore(myApp);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+document.getElementById('root'));
